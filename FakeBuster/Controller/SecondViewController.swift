2a729da419545651d5566f3b2de4a94aadda1e47
Robert Pelka <123455ff@gmail.com>
2021-03-11 07:56:39 +0100

    Add secondView and app icon, fix detective on the firstView

--- FakeBuster/Controller/SecondViewController.swift
+++ FakeBuster/Controller/SecondViewController.swift
@@ -1,25 +1,29 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  FakeBuster
 //
-//  Created by Robert Pelka on 07/03/2021.
+//  Created by Robert Pelka on 11/03/2021.
 //
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var chooseButton: UIButton!
     @IBOutlet weak var pasteButton: UIButton!
     @IBOutlet weak var checkButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         backgroundView.layer.cornerRadius = 15
+        imageView.layer.cornerRadius = 5
+        chooseButton.layer.cornerRadius = 13
         pasteButton.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         pasteButton.layer.cornerRadius = 13
         checkButton.layer.cornerRadius = 10
     }
-    
+
 }
