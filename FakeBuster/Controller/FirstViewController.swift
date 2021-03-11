4eb9fe14f40f9f7b84da6365bb3fe6a86e179ddd
Robert Pelka <123455ff@gmail.com>
2021-03-11 14:34:35 +0100

    Make "paste" and "choose" buttons work

--- FakeBuster/Controller/FirstViewController.swift
+++ FakeBuster/Controller/FirstViewController.swift
@@ -12,6 +12,7 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var pasteButton: UIButton!
     @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var urlTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,13 @@ class FirstViewController: UIViewController {
         pasteButton.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         pasteButton.layer.cornerRadius = 13
         checkButton.layer.cornerRadius = 10
+        
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @IBAction func pasteButtonPressed(_ sender: UIButton) {
+        urlTextField.text = UIPasteboard.general.string
     }
     
 }
--- FakeBuster/Controller/SecondViewController.swift
+++ FakeBuster/Controller/SecondViewController.swift
@@ -15,9 +15,14 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var pasteButton: UIButton!
     @IBOutlet weak var checkButton: UIButton!
     
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        
         backgroundView.layer.cornerRadius = 15
         imageView.layer.cornerRadius = 5
         chooseButton.layer.cornerRadius = 13
@@ -25,5 +30,25 @@ class SecondViewController: UIViewController {
         pasteButton.layer.cornerRadius = 13
         checkButton.layer.cornerRadius = 10
     }
+    
+    @IBAction func pasteButtonPressed(_ sender: UIButton) {
+        if let pasteboardImage = UIPasteboard.general.image {
+            imageView.image = pasteboardImage
+        }
+    }
 
+}
+
+extension SecondViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBAction func chooseButtonPressed(_ sender: UIButton) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let choosenImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = choosenImage
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+    }
 }
